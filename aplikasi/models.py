e95fae72491c6917f1ba78d566d23729c2da436e
bLack-Domon <jakfar.ilham23@gmail.com>
2025-02-23 12:04:56 +0700

    update qrcode

--- aplikasi/models.py
+++ aplikasi/models.py
@@ -1,4 +1,8 @@
+import qrcode
+from io import BytesIO
+from django.core.files.base import ContentFile
 from django.db import models
+from django.utils.timezone import now
 
 class Siswa(models.Model):
     nama = models.CharField(max_length=100)
@@ -6,6 +10,16 @@ class Siswa(models.Model):
     foto = models.ImageField(upload_to='foto_siswa/', blank=True, null=True)
     qrcode = models.ImageField(upload_to='qrcodes/', blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        qr_data = f"{self.nama} - {self.jurusan}"
+        qr = qrcode.make(qr_data)
+
+        buffer = BytesIO()
+        qr.save(buffer, format='PNG')
+        filename = f'qr-{self.nama}.png'
+        self.qrcode.save(filename, ContentFile(buffer.getvalue()), save=False)
+
+        super().save(*args, **kwargs)
     def __str__(self):
         return self.nama
 
